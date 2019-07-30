fc03ce1d902931733ea1041076661f793534ca52
Konstantin <balashov-92@mail.ru>
2019-07-30 20:30:02 +0300

    Game Stickman from book "Python to kids". Игра "Человечек спешит к выходу" из книги Пайтон для детей.

---
+++ Stickman/глава 17 Упр2.py
@@ -0,0 +1,36 @@
+Python
+3.7
+.0(v3
+.7
+.0: 1
+bf9cc5093, Jun
+27
+2018, 04: 06:47) [MSC v.1914 32 bit(Intel)]
+on
+win32
+Type
+"copyright", "credits" or "license()"
+for more information.
+    >> >
+    RESTART: C: / Users / Константин / Desktop / Python / Человечек
+    спешит
+    к
+    выходу / stickman / Глава
+    17
+    упражнение
+    game.py
+    Traceback(most
+    recent
+    call
+    last):
+    File
+    "C:/Users/Константин/Desktop/Python/Человечек спешит к выходу/stickman/Глава 17 упражнение game.py", line
+    155, in < module >
+    g.mainloop()
+    AttributeError: 'Game'
+    object
+    has
+    no
+    attribute
+    'mainloop'
+    >> >
