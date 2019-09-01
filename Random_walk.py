693bfe1b5cb5339dc4f69af6e9ccb05993d0a1be
Konstantin <balashov-92@mail.ru>
2019-09-01 21:51:38 +0300

    Bag fixed

--- Random_walk.py
+++ Random_walk.py
@@ -3,7 +3,7 @@ from random import choice
 
 class RandomWalk:
 
-    def __init__(self, num_points=500):
+    def __init__(self, num_points=5000):
         self.num_points = num_points
         self.x_values = [0]
         self.y_values = [0]
@@ -15,7 +15,7 @@ class RandomWalk:
             x_step = x_direction * x_distance
             y_direction = choice([1, -1])
             y_distance = choice([0, 1, 2, 3, 4])
-            y_step = x_direction * x_distance
+            y_step = y_direction * y_distance
             if x_step == 0 and y_step == 0:
                 continue
             next_x = self.x_values[-1] + x_step
--- rw_visual.py
+++ rw_visual.py
@@ -8,13 +8,16 @@ from Random_walk import RandomWalk
 # plt.show()
 colors = ['red', 'blue', 'green', 'black', 'yellow']
 while True:
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
+    plt.figure(dpi=128, figsize=(10, 6))
     rw.fill_walk()
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers,
-                cmap=plt.cm.Blues, edgecolor=None, s=15)
+                cmap=plt.cm.Blues, edgecolor=None, s=1)
     plt.scatter(0, 0, c='green', s=100)
     plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', s=100)
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
     plt.show()
     keep_running = input('Make another walk? y/n: ')
     if keep_running == 'n':
