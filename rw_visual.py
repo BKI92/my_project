f9a9f3bbd5bedc19ad7ee0eacedb09fa21d089d0
Konstantin <balashov-92@mail.ru>
2019-09-01 21:49:36 +0300

    Теперь выделены точки с начальынм и конечным блужданием

--- rw_visual.py
+++ rw_visual.py
@@ -2,7 +2,22 @@ import matplotlib.pyplot as plt
 from Random_walk import RandomWalk
 
 
-rw = RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+# rw = RandomWalk()
+# rw.fill_walk()
+# plt.scatter(rw.x_values, rw.y_values, s=15)
+# plt.show()
+colors = ['red', 'blue', 'green', 'black', 'yellow']
+while True:
+    rw = RandomWalk()
+    rw.fill_walk()
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers,
+                cmap=plt.cm.Blues, edgecolor=None, s=15)
+    plt.scatter(0, 0, c='green', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', s=100)
+    plt.show()
+    keep_running = input('Make another walk? y/n: ')
+    if keep_running == 'n':
+        break
+
+
