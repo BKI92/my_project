77d9db013c6a99dd1f83c7e76ba6e1f4e5beec8b
Konstantin <balashov-92@mail.ru>
2019-10-10 15:20:13 +0300

    Исправлена ошибка в вычислениях

--- leap_hackerrank.py
+++ leap_hackerrank.py
@@ -1,19 +1,17 @@
 def is_leap(year):
-    leap = False
+    leap = True
 
     if year % 4 == 0:
-        leap = True
         if year % 100 == 0:
             if year % 400 == 0:
                 leap = True
             else:
                 leap = False
-        else:
-            leap = False
-
+    else:
+        leap = False
     return leap
 
 
 for year in range(1800, 10000):
-    print(f'||year-{year:^4}|%4-{year%4:^2}|%100-{year%100:^2}|%400-{year%6:^2}|{is_leap(year)}||')
-    print('-' * 54)
+    if is_leap(year):
+        print(year)
